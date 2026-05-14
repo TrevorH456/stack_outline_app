b2ece0242d838ebbe5008a6d4b7872c2613b7e44
Trevor Herdman <herdmantrevor@gmail.com>
2026-05-14 17:43:35 -0500

    Initial Testing Commit

--- app/models.py
+++ app/models.py
@@ -7,3 +7,50 @@ class ExampleRecord(db.Model):
 
     def __repr__(self):
         return f"<ExampleRecord {self.title}>"
+
+class Members(db.Model):
+    __tablename__ = "members"
+
+    member_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    membership_level = db.Column(db.String(30), nullable=False)
+    join_date = db.Column(db.Date, nullable=False)
+    created_date = db.Column(db.Date, nullable=False)
+
+
+class Trainers(db.Model):
+    __tablename__ = "trainers"
+
+    trainer_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    join_date = db.Column(db.Date, nullable=False)
+    created_date = db.Column(db.Date, nullable=False)
+
+
+class Classes(db.Model):
+    __tablename__ = "classes"
+
+    class_id = db.Column(db.Integer, primary_key=True)
+    class_name = db.Column(db.String(100), nullable=False)
+    class_date = db.Column(db.Date, nullable=False)
+    class_time = db.Column(db.Time, nullable=False)
+    trainer_id = db.Column(db.Integer, db.ForeignKey("trainers.trainer_id"), nullable=False)
+    created_date = db.Column(db.Date, nullable=False)
+
+
+class Registration(db.Model):
+    __tablename__ = "registration"
+
+    registration_id = db.Column(db.Integer, primary_key=True)
+    class_id = db.Column(db.Integer, db.ForeignKey("classes.class_id"), nullable=False)
+    member_id = db.Column(db.Integer, db.ForeignKey("members.member_id"), nullable=False)
+    registration_date = db.Column(db.Date, nullable=False)
+    created_date = db.Column(db.Date, nullable=False)
+
+    __table_args__ = (
+        db.UniqueConstraint("class_id", "member_id", name="unique_member_class_registration"),
+    )
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,23 @@
 from flask import Blueprint, render_template
 
-from .models import ExampleRecord
-
+from .models import (
+    Members,
+    Trainers,
+    Classes,
+    Registration
+)
 main = Blueprint("main", __name__)
-
 
 @main.route("/")
 def index():
-    records = ExampleRecord.query.order_by(ExampleRecord.id.desc()).all()
-    return render_template("index.html", records=records)
+    members = Members.query.all()
+    trainers = Trainers.query.all()
+    classes = Classes.query.all()
+    registrations = Registration.query.all()
+    return render_template(
+        "index.html",
+        members=members,
+        trainers=trainers,
+        classes=classes,
+        registrations=registrations
+    )
